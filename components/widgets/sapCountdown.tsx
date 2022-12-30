c1a3a2b1427dbd767e8f2166d54ac67f1a552448
KadonWills <kapolw@gmail.com>
2022-12-30 14:40:50 +0100

    date display update

--- components/widgets/sapCountdown.tsx
+++ components/widgets/sapCountdown.tsx
@@ -24,13 +24,13 @@ const SapCountdown = () => {
                                 Start Up's Registration Deadline
                             </h6>
                             <h3 className="font-bold font-heading text-2xl md:text-3xl text-white">
-                                30 <sup >th</sup> December 2022
+                                15 <sup >th</sup> January 2023
                             </h3>
                             <h3 className="font-bold font-heading text-lg md:text-xl text-white leading-tight">
                                 @ 12:00 AM
                             </h3>
                             <div className="mt-4 w-full md:w-44">
-                                <a href='https://docs.google.com/forms/d/1m3S9AKbFxEXd2VIdyampszRbF_UNP-7IK8n3JeDipi4' target={'_blank'} className="py-2 px-4  bg-white hover:bg-gray-100 focus:ring-blue-500 focus:ring-offset-blue-200 text-blue-500 hover:bg-blue-500 hover:text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                <a href='https://docs.google.com/forms/d/1m3S9AKbFxEXd2VIdyampszRbF_UNP-7IK8n3JeDipi4' target={'_blank'} className="py-2 px-4  bg-white hover:bg-yellow-400 hover:text-slate-800 focus:ring-blue-500 focus:ring-offset-blue-200 text-blue-500 hover:bg-blue-500 hover:text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-full ">
                                     Register Now
                                 </a>
                             </div>
